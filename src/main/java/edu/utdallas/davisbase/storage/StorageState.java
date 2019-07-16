322640636d5fb06e7ff01327f599ea1574521770
Travis C. LaGrone <22419287+travis-c-lagrone@users.noreply.github.com>
2019-07-15 19:07:05 -0500

    Fill in definition of class 'StorageState'

--- src/main/java/edu/utdallas/davisbase/storage/StorageState.java
+++ src/main/java/edu/utdallas/davisbase/storage/StorageState.java
@@ -1,5 +1,54 @@
 package edu.utdallas.davisbase.storage;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.io.File;
+
+import org.checkerframework.checker.nullness.qual.Nullable;
+
 public class StorageState {
-  // TODO Implement StorageState
+
+  public static class Builder {
+
+    public static File getDefaultDataDirectory() {
+      return new File(System.getenv("user.dir"));
+    }
+
+    private @Nullable File dataDirectory = null;
+
+    public Builder() {}
+
+    public void setDataDirectory(String dataDirectory) {
+      checkNotNull(dataDirectory);
+      File file = new File(dataDirectory);
+
+      checkArgument(file.exists(), "Data directory path does not exist");
+      checkArgument(file.isDirectory(), "Data directory path is not a directory");
+
+      this.dataDirectory = file;
+    }
+
+    public StorageState build() {
+      File dataDirectory = getDefaultDataDirectory();
+      if (this.dataDirectory != null) {
+        dataDirectory = this.dataDirectory;
+      }
+
+      return new StorageState(dataDirectory);
+    }
+  }
+
+  private File dataDirectory;
+
+  private StorageState(File dataDirectory) {
+    this.dataDirectory = dataDirectory;
+  }
+
+  /**
+   * @return the dataDirectory
+   */
+  public File getDataDirectory() {
+    return dataDirectory;
+  }
 }
