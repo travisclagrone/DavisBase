f9618d5c23a9423c8b759c4eb0a4010f1659a4bf
Travis C. LaGrone <22419287+travis-c-lagrone@users.noreply.github.com>
2019-07-15 19:10:37 -0500

    Remove outdated architecture diagrams

--- src/main/java/edu/utdallas/davisbase/storage/Storage.java
+++ src/main/java/edu/utdallas/davisbase/storage/Storage.java
@@ -5,7 +5,7 @@ public class Storage {
   private final StorageConfiguration configuration;
   private final StorageState state;
 
-  private Storage(StorageConfiguration configuration, StorageState state) {
+  public Storage(StorageConfiguration configuration, StorageState state) {
     this.configuration = configuration;
     this.state = state;
   }
