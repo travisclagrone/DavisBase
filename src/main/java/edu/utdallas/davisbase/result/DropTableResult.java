7aa0b1dc9c56ed9e913ff00b0285916f7a8e7d76
Travis C. LaGrone <22419287+travis-c-lagrone@users.noreply.github.com>
2019-07-17 14:49:57 -0500

    Implement class edu.utdallas.davisbase.result.DropTableResult

--- src/main/java/edu/utdallas/davisbase/result/DropTableResult.java
+++ src/main/java/edu/utdallas/davisbase/result/DropTableResult.java
@@ -1,5 +1,49 @@
 package edu.utdallas.davisbase.result;
 
+import static com.google.common.base.MoreObjects.toStringHelper;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static java.util.Objects.hash;
+
 public class DropTableResult implements Result {
-  // TODO Implement DropTableResult
+
+  private final String tableName;
+
+  /**
+   * @param tableName the name of the table that was dropped
+   */
+  public DropTableResult(String tableName) {
+    checkNotNull(tableName);
+
+    this.tableName = tableName;
+  }
+
+  /**
+   * @return the name of the table that was dropped
+   */
+  public String getTableName() {
+    return tableName;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj != null && obj instanceof DropTableResult)) {
+      return false;
+    }
+
+    DropTableResult other = (DropTableResult) obj;
+    return getTableName().equals(other.getTableName());
+  }
+
+  @Override
+  public int hashCode() {
+    return hash(getTableName());
+  }
+
+  @Override
+  public String toString() {
+    return toStringHelper(DropTableResult.class)
+        .add("tableName", getTableName())
+        .toString();
+  }
+
 }
