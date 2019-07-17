5faf81a4cfe23fa2129e3ef50165fb3a2749b628
Travis C. LaGrone <22419287+travis-c-lagrone@users.noreply.github.com>
2019-07-17 14:35:04 -0500

    Implement class edu.utdallas.davisbase.result.CreateTableResult

--- src/main/java/edu/utdallas/davisbase/result/CreateTableResult.java
+++ src/main/java/edu/utdallas/davisbase/result/CreateTableResult.java
@@ -1,5 +1,49 @@
 package edu.utdallas.davisbase.result;
 
+import static java.util.Objects.hash;
+import static com.google.common.base.MoreObjects.toStringHelper;
+import static com.google.common.base.Preconditions.checkNotNull;
+
 public class CreateTableResult implements Result {
-  // TODO Implement CreateTableResult
+
+  private final String tableName;
+
+  /**
+   * @param tableName the name of the table that was created (not null)
+   */
+  public CreateTableResult(String tableName) {
+    checkNotNull(tableName);
+
+    this.tableName = tableName;
+  }
+
+  /**
+   * @return the name of the table that was created (not null)
+   */
+  public String getTableName() {
+    return tableName;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj != null && obj instanceof CreateTableResult)) {
+      return false;
+    }
+
+    CreateTableResult other = (CreateTableResult) obj;
+    return getTableName().equals(other.getTableName());
+  }
+
+  @Override
+  public int hashCode() {
+    return hash(tableName);
+  }
+
+  @Override
+  public String toString() {
+    return toStringHelper(CreateTableResult.class)
+        .add("tableName", getTableName())
+        .toString();
+  }
+
 }
