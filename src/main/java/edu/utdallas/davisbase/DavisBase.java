3363d4fd4530b062e2274f551cdbec3ebd62d8c4
hitharr <hitharr@users.noreply.github.com>
2019-07-21 15:52:25 -0500

    revert DavisBase class

--- src/main/java/edu/utdallas/davisbase/DavisBase.java
+++ src/main/java/edu/utdallas/davisbase/DavisBase.java
@@ -1,10 +1,5 @@
 package edu.utdallas.davisbase;
 
-import edu.utdallas.davisbase.parser.ParseException;
-import edu.utdallas.davisbase.parser.Parser;
-import edu.utdallas.davisbase.parser.ParserConfiguration;
-import edu.utdallas.davisbase.representation.CommandRepresentation;
-
 public class DavisBase {
 
   public static void main(String[] args) {
@@ -24,36 +19,6 @@ public class DavisBase {
    */
   public int run() {
     // TODO Implement DavisBase.run()
-    //call Parser.parse(input)
-    Parser parse = new Parser(new ParserConfiguration());
-    try{
-      CommandRepresentation rep;
-//      rep = parse.parse("CREATE TABLE Persons (PersonID int, LastName varchar(255), FirstName varchar(255));");
-//     rep= parse.parse("DROP TABLE Shippers;");
-//      rep = parse.parse("CREATE INDEX idx_lastname ON Persons;");
-//      rep=parse.parse("DELETE FROM Customers WHERE CustomerName='Alfreds Futterkiste';");
-//      rep=parse.parse("DELETE FROM Customers WHERE NOT id <> 5;");
-//       rep = parse.parse("SELECT age+16 FROM Customers;");
-//      rep = parse.parse("SELECT * FROM Customers;");
-//        rep= parse.parse("SELECT col1, col2 FROM Customers;");
-//      rep=parse.parse("UPDATE Customers\n" +
-//        "SET ContactName = 'Alfred Schmidt', City= 'Frankfurt'\n" +
-//        "WHERE NOT CustomerID =5;");
-//      rep=parse.parse("UPDATE Customers\n" +
-//        "SET ContactName = 'Alfred Schmidt', City= 'Frankfurt'\n" +
-//        "WHERE CustomerID IN  ('1','2','3');");
-//      parse.parse("SELECT * FROM Customers\n" +
-//        "WHERE NOT Country='Germany';");
-//    rep= parse.parse("INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)\n" +
-//      "VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway');");
-//      rep= parse.parse("EXIT;");
-//      rep= parse.parse("  show      tables;  ");
-      System.out.println(rep);
-    }
-    catch (ParseException e){
-      System.out.println(e.getCause());
-    }
-//    throw new NotImplementedException();
-    return 0;
+    throw new NotImplementedException();
   }
 }
