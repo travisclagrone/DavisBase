95bd019b2c99d3d2798be6b1f3a4264503a4ece1
Travis C. LaGrone <22419287+travis-c-lagrone@users.noreply.github.com>
2019-07-23 17:25:58 -0500

    Implement enum DavisBaseColumnsTableColumn in package edu.utdallas.davisbase.catalog

---
+++ src/main/java/edu/utdallas/davisbase/catalog/DavisBaseTablesTableColumn.java
@@ -0,0 +1,46 @@
+package edu.utdallas.davisbase.catalog;
+
+import static java.lang.String.format;
+
+import edu.utdallas.davisbase.DataType;
+
+/**
+ * An enumerated column schema for the {@link CatalogTable#DAVISBASE_TABLES davisbase_tables}
+ * catalog table.
+ */
+public enum DavisBaseTablesTableColumn implements CatalogTableColumn {
+  ROWID      (DataType.INT,  false),
+  TABLE_NAME (DataType.TEXT, false);
+
+  private final DataType dataType;
+  private final boolean isNullable;
+
+  private DavisBaseTablesTableColumn(DataType dataType, boolean isNullable) {
+    assert dataType != null : "dataType should not be null";
+    assert ordinal() < Byte.MAX_VALUE : format("ordinal() should be less than %d", Byte.MAX_VALUE);
+
+    this.dataType = dataType;
+    this.isNullable = isNullable;
+  }
+
+  @Override
+  public String getName() {
+    return name().toLowerCase();
+  }
+
+  @Override
+  public DataType getDataType() {
+    return dataType;
+  }
+
+  @Override
+  public byte getOrdinalPosition() {
+    return (byte) ordinal();
+  }
+
+  @Override
+  public boolean isNullable() {
+    return isNullable;
+  }
+
+}
