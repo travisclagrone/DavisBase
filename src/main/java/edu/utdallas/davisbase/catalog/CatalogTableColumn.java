ec1f40fdc4491c986062a7831f86baf3ed7665b0
Travis C. LaGrone <22419287+travis-c-lagrone@users.noreply.github.com>
2019-07-23 17:25:08 -0500

    Document interface CatalogTableColumn in package edu.utdallas.davisbase.catalog

--- src/main/java/edu/utdallas/davisbase/catalog/CatalogTableColumn.java
+++ src/main/java/edu/utdallas/davisbase/catalog/CatalogTableColumn.java
@@ -3,16 +3,30 @@ package edu.utdallas.davisbase.catalog;
 import edu.utdallas.davisbase.DataType;
 
 /**
- * A column schema for a catalog table.
+ * A column schema for a {@link CatalogTable}.
  */
 public interface CatalogTableColumn {
 
+  /**
+   * @return the canonical name of this column (not null)
+   */
   public String getName();
 
+  /**
+   * @return the {@link DataType} of this column (not null)
+   */
   public DataType getDataType();
 
+  /**
+   * @return the zero-based ordinal position of this column in the table to which it belongs (not
+   *         negative, less than {@link java.lang.Byte#MAX_VALUE Byte.MAX_VALUE}, and unique within
+   *         a table)
+   */
   public byte getOrdinalPosition();
 
+  /**
+   * @return {@code true} if-and-only-if this column accepts DavisBase {@code null} values
+   */
   public boolean isNullable();
 
 }
