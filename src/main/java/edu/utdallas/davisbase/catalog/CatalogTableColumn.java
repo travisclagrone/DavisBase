bc09578b37b2f778dba679a16ef39b7726e89a57
Travis C. LaGrone <22419287+travis-c-lagrone@users.noreply.github.com>
2019-07-23 14:38:38 -0500

    Implement interface CatalogTableColumn in package edu.utdallas.davisbase.catalog

---
+++ src/main/java/edu/utdallas/davisbase/catalog/CatalogTableColumn.java
@@ -0,0 +1,20 @@
+package edu.utdallas.davisbase.catalog;
+
+import edu.utdallas.davisbase.DataType;
+
+/**
+ * A column schema for a catalog table.
+ */
+public interface CatalogTableColumn {
+
+  public CatalogTable getTable();
+
+  public String getName();
+
+  public DataType getDataType();
+
+  public byte getOrdinalPosition();
+
+  public boolean isNullable();
+
+}
