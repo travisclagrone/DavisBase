d37b968b6780a8ca6db901220ef760d1f11f2c67
Travis C. LaGrone <22419287+travis-c-lagrone@users.noreply.github.com>
2019-07-29 11:22:30 -0500

    Implement class DropTableCommand in package edu.utdallas.davisbase.command

--- src/main/java/edu/utdallas/davisbase/command/DropTableCommand.java
+++ src/main/java/edu/utdallas/davisbase/command/DropTableCommand.java
@@ -1,13 +1,50 @@
 package edu.utdallas.davisbase.command;
 
-@SuppressWarnings("nullness")  // COMBAK Unsuppress nullness warnings once we implement DropTableCommand.
+import static java.util.Objects.hash;
+
+import static com.google.common.base.MoreObjects.toStringHelper;
+import static com.google.common.base.Preconditions.checkNotNull;
+
 public class DropTableCommand implements Command {
 
-  private String tableName;
+  private final String tableName;
 
-  // COMBAK Implement DropTableCommand
-
+  /**
+   * @param tableName the name of the table to drop (not null)
+   */
   public DropTableCommand(String tableName) {
+    checkNotNull(tableName, "tableName");
+
     this.tableName = tableName;
   }
+
+  /**
+   * @return the name of the table to drop (not null)
+   */
+  public String getTableName() {
+    return tableName;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj != null && obj instanceof DropTableCommand)) {
+      return false;
+    }
+
+    DropTableCommand other = (DropTableCommand) obj;
+    return getTableName().equals(other.getTableName());
+  }
+
+  @Override
+  public int hashCode() {
+    return hash(getTableName());
+  }
+
+  @Override
+  public String toString() {
+    return toStringHelper(DropTableCommand.class)
+        .add("tableName", getTableName())
+        .toString();
+  }
+
 }
