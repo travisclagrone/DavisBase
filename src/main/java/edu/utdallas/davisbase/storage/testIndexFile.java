aac68f5987a990f392ec51a608fa4a2cdb513536
bhrao <bhraokgp@gmail.com>
2019-07-31 19:24:45 -0500

    testing Index file method commited

---
+++ src/main/java/edu/utdallas/davisbase/storage/testIndexFile.java
@@ -0,0 +1,24 @@
+package edu.utdallas.davisbase.storage;
+
+import java.io.RandomAccessFile;
+
+public class testIndexFile {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    try {
+
+      RandomAccessFile file;
+      file = new RandomAccessFile(
+          "C:\\Users\\Hanumantha Rao\\Documents\\Semesters\\Summer 2019\\CS 6360 - DB Design\\Projects\\Part 1\\DavisBase\\data\\test1.tbl", "rw");
+      IndexFile iFile = new IndexFile(file);
+      System.out.println("happy");
+      iFile.addText("hanu", 10);
+      System.out.print("happy");
+    } catch (Exception e) {
+
+    }
+
+  }
+
+}
